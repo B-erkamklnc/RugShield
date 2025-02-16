2e1674b720a30794a130f2b0b2449c934d643212
erkam <101091269+B-erkamklnc@users.noreply.github.com>
2025-02-16 05:41:30 +0300

    Create RugShield.sol

---
+++ RugShield.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.8.26;
+
+interface IUniswapV2Pair {
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);   
+}
